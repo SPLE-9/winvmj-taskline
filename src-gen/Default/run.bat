3f7666b0d7d02f97de2ce7d6866508942b3c4b78
NI04Code <naufalichsan114@gmail.com>
2025-05-25 19:29:48 +0700

    add timelogduration to feature to module json

--- src-gen/Default/run.bat
+++
@@ -1,4 +0,0 @@
-echo SELECT 'CREATE DATABASE taskline_product_default' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'taskline_product_default') \gexec | psql "postgresql://postgres:postgres@localhost"
-for %%G in (sql/*.sql) do psql -a -f sql/%%G "postgresql://postgres:postgres@localhost/taskline_product_default"
-
-java -cp taskline.product.default --module-path taskline.product.default -m taskline.product.default
